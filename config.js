93cb1274a12fd1ad10f24bcaca63043432672005
chessplayer-mimi <369starofdavid@gmail.com>
2025-03-06 01:14:01 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
-export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFwkjeLA8VjI1MFflpECFNz-hV6S8ZyPw",
+  authDomain: "smash-u-in-life.firebaseapp.com",
+  databaseURL: "https://smash-u-in-life-default-rtdb.firebaseio.com",
+  projectId: "smash-u-in-life",
+  storageBucket: "smash-u-in-life.firebasestorage.app",
+  messagingSenderId: "601839091807",
+  appId: "1:601839091807:web:e735bb69cfa15ba3369bea",
+  measurementId: "G-NFHJEJB56Z"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
